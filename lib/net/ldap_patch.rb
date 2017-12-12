422b017a242082fd5f59e3a283ca6de403963e8a
Chris Bucchere <chris@bucchere.com>
2017-12-12 15:45:30 -0800

    Replace alias_method_chain with prepend

--- lib/net/ldap_patch.rb
+++ lib/net/ldap_patch.rb
@@ -1,32 +1,29 @@
 require 'net/ldap'
-
-if defined?(Net::LDAP)
-  class Net::LDAP
-    def open_with_persistent_connection(*args, &block)
-      if block_given?
-        open_without_persistent_connection(*args, &block)
-      else
-        begin
-          local_connection = nil
-          open_without_persistent_connection(*args) do |ldap|
-            local_connection = @open_connection
-            @open_connection = nil
-          end
-        ensure
-          @open_connection = local_connection
+class Net::LDAP
+  original_open_method = instance_method(:open)
+  
+  define_method(:open) do |*args, &block|
+    if block_given?
+      original_open_method.bind(self).(*args, &block)
+    else
+      begin
+        local_connection = nil
+        original_open_method.bind(self).(*args) do |ldap|
+          local_connection = @open_connection
+          @open_connection = nil
         end
+      ensure
+        @open_connection = local_connection
       end
     end
+  end
 
-    def closed?
-      !@open_connection
-    end
+  define_method(:closed?) do
+    !@open_connection
+  end
 
-    def close
-      @open_connection.close unless closed?
-      @open_connection = nil
-    end
-
-    alias_method_chain :open, :persistent_connection
+  define_method(:close) do
+    @open_connection.close unless closed?
+    @open_connection = nil
   end
 end
